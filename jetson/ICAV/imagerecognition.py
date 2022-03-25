ecdc344f0400817fd3bd5e4d37c1c3fd2fae771e
jeremy <jeremysee_2@hotmail.com>
2022-03-25 17:04:34 +0000

    added test code for development

--- jetson/ICAV/imagerecognition.py
+++ jetson/ICAV/imagerecognition.py
@@ -9,8 +9,11 @@ class ImageRecognition():
     def __init__(self) -> None:
         self.altitude   = ICAV.altitude.Altitude()
         self.GPS        = ICAV.GPSposition.GPSPosition()
-        self.image      = None
+        self.img        = None
         self.targetGPS  = None
+
+        # Prepare OpenCV
+
 
     def captureImage(self) -> None:
         pass
@@ -24,3 +27,39 @@ class ImageRecognition():
     """
     def calculateTargetGPS(self) -> Tuple[float, float]:
         pass
+
+    """
+    Preprocessing function to increase brightness of original image:
+    """
+    def increaseBrightness(self, value=30) -> None:
+        hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(hsv)
+
+        lim = 255 - value
+        v[v > lim] = 255
+        #v[v <= lim] += value
+
+        final_hsv = cv2.merge((h, s, v))
+        self.img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+
+    """
+    Debugging: set a dummy image for testing
+    """
+    def setImage(self, image) -> None:
+        self.img = image
+
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+    imgrec = ImageRecognition()
+
+    # Read in test image
+    img_original = cv2.imread('IMG_3084.JPG')
+    imgrec.setImage(img_original)
+
+    # Test target recognition
+    imgrec.calculateTargetInImage()
+
+    # Test GPS calculation
+    lat, long = imgrec.calculateTargetGPS()
+    print(f'Target lat, long: ({lat}, {long})')
